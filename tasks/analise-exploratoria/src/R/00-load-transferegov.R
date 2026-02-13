cda910ad179db4e14cb766cc692c278f0d9ed725
rdurl0 <durlo.raul@gmail.com>
2026-02-13 11:43:31 -0300

    feat: refatora função de consolidação de emendas e ajusta opções de exibição

--- tasks/analise-exploratoria/src/R/00-load-transferegov.R
+++ tasks/analise-exploratoria/src/R/00-load-transferegov.R
@@ -1,6 +1,6 @@
 library(here)
 library(tidyverse)
-options(width = 150)
+options(width = 180)
 
 # :: PATHS ---------------------------------------------------------------------
 
@@ -57,121 +57,133 @@ transferegov <- read_transferegov_csvs(PATH_TRANSFEREGOV_TABLES)
 
 # :: SELECIONA TABELAS E CAMPOS ------------------------------------------------
 
-# Plano de ação
-# -------------
-plano_acao <- transferegov$plano_acao |>
-  filter(id_programa == "23") |>
-  mutate(across(starts_with("valor_"), as.numeric)) |>
-  select(
-    id_plano_acao,
-    cnpj_beneficiario_plano_acao,
-    nome_beneficiario_plano_acao,
-    uf_beneficiario_plano_acao,
-    codigo_emenda_parlamentar_formatado_plano_acao,
-    valor_custeio_plano_acao,
-    valor_investimento_plano_acao
-  )
+consolida_emendas_por_programas <- function(id_prog) {
 
-# Executores
-# ----------
-executores <- plano_acao |>
-  distinct(id_plano_acao) |>
-  inner_join(transferegov$executor) |>
-  mutate(across(starts_with("vl_"), as.numeric)) |>
-  distinct(
-    id_plano_acao,	id_executor,	cnpj_executor, nome_executor,	objeto_executor,
-    vl_custeio_executor, vl_investimento_executor
-  )
+  # Plano de ação
+  # -------------
+  plano_acao <- transferegov$plano_acao |>
+    filter(id_programa == id_prog) |>
+    mutate(across(starts_with("valor_"), as.numeric)) |>
+    left_join(select(transferegov$programa, id_programa, codigo_programa)) |>
+    select(
+      codigo_programa, id_plano_acao,
+      cnpj_beneficiario_plano_acao,
+      nome_beneficiario_plano_acao,
+      uf_beneficiario_plano_acao,
+      codigo_emenda_parlamentar_formatado_plano_acao,
+      valor_custeio_plano_acao,
+      valor_investimento_plano_acao
+    )
 
-# Metas
-# -----
-metas <- plano_acao |>
-  distinct(id_plano_acao) |>
-  inner_join(executores) |>
-  distinct(id_executor) |>
-  inner_join(transferegov$meta) |>
-  mutate(across(starts_with("vl_"), as.numeric)) |>
-  mutate(across(starts_with("qt_"), as.numeric)) |>
-  select(-nome_meta)
+  # Executores
+  # ----------
+  executores <- plano_acao |>
+    distinct(id_plano_acao) |>
+    inner_join(transferegov$executor) |>
+    mutate(across(starts_with("vl_"), as.numeric)) |>
+    distinct(
+      id_plano_acao, id_executor,
+      cnpj_executor, nome_executor,	objeto_executor,
+      vl_custeio_executor, vl_investimento_executor
+    )
 
-# Plano de trabalho
-# -----------------
-plano_trabalho <- plano_acao |>
-  distinct(id_plano_acao) |>
-  inner_join(transferegov$plano_trabalho) |>
-  select(
-    id_plano_acao, id_plano_trabalho, situacao_plano_trabalho,
-    prazo_execucao_meses_plano_trabalho, classificacao_orcamentaria_pt
-  )
+  # Metas
+  # -----
+  metas <- plano_acao |>
+    distinct(id_plano_acao) |>
+    inner_join(executores) |>
+    distinct(id_executor) |>
+    inner_join(transferegov$meta) |>
+    mutate(across(starts_with("vl_"), as.numeric)) |>
+    mutate(across(starts_with("qt_"), as.numeric)) |>
+    select(-nome_meta)
 
-# Finalidades
-# -----------
-finalidades <- executores |>
-  distinct(id_executor) |>
-  inner_join(transferegov$finalidade)
-  # não tá fazendo muito sentido: ver id_executor == "47042"
+  # Plano de trabalho
+  # -----------------
+  plano_trabalho <- plano_acao |>
+    distinct(id_plano_acao) |>
+    inner_join(transferegov$plano_trabalho) |>
+    select(
+      id_plano_acao, id_plano_trabalho, situacao_plano_trabalho,
+      prazo_execucao_meses_plano_trabalho, classificacao_orcamentaria_pt
+    )
+
+  # Finalidades
+  # -----------
+  finalidades <- executores |>
+    distinct(id_executor) |>
+    inner_join(transferegov$finalidade)
+    # não tá fazendo muito sentido: ver id_executor == "47042"
 
 
-# :: CRIAR TABELA ÚNICA --------------------------------------------------------
+  # :: CRIAR TABELA ÚNICA --------------------------------------------------------
 
-plano_acao # 10.145 linhas
-plano_trabalho # 9.747 linhas
-executores # 9.753 linhas
-metas # 23.701 linhas (out.2025) | 25.240 (fev.2025)
-# finalidades # 10.349 linhas
+  plano_acao # 10.145 linhas
+  plano_trabalho # 9.747 linhas
+  executores # 9.753 linhas
+  metas # 23.701 linhas (out.2025) | 25.240 (fev.2025)
+  # finalidades # 10.349 linhas
 
-# RELAÇÕES:
-# Um plano de ação contém um ou nenhum planos de trabalho
-# Um plano de ação contém um ou mais executores
-# Um executor contém uma ou mais metas
+  # RELAÇÕES:
+  # Um plano de ação contém um ou nenhum planos de trabalho
+  # Um plano de ação contém um ou mais executores
+  # Um executor contém uma ou mais metas
 
-emendas <- plano_acao |>
-  left_join(plano_trabalho) |>
-  left_join(executores)
+  emendas <- plano_acao |>
+    left_join(plano_trabalho) |>
+    left_join(executores)
 
-emendas <- left_join(emendas, metas)
+  emendas <- left_join(emendas, metas)
 
-emendas <- emendas |>
-  select(
-    id_plano_acao,
-    id_executor,
-    id_plano_trabalho,
-    id_meta,
-    sequencial_meta,
-    codigo_emenda_parlamentar_formatado_plano_acao,
-    cnpj_beneficiario_plano_acao,
-    nome_beneficiario_plano_acao,
-    uf_beneficiario_plano_acao,
-    cnpj_executor,
-    nome_executor,
-    objeto_executor,
-    situacao_plano_trabalho,
-    prazo_execucao_meses_plano_trabalho,
-    classificacao_orcamentaria_pt,
-    desc_meta,
-    qt_meses_meta,
-    un_medida_meta,
-    qt_unidade_meta = qt_uniade_meta,
-    #
-    valor_custeio_plano_acao,
-    valor_investimento_plano_acao,
-    #
-    vl_custeio_executor,
-    vl_investimento_executor,
-    #
-    vl_custeio_emenda_especial_meta,
-    vl_investimento_emenda_especial_meta,
-    #
-    vl_custeio_recursos_proprios_meta,
-    vl_investimento_recursos_proprios_meta,
-    #
-    vl_custeio_doacao_meta,
-    vl_investimento_doacao_meta,
-    #
-    vl_custeio_rendimento_meta,
-    vl_investimento_rendimento_meta
+  emendas |>
+    select(
+      codigo_programa,
+      id_plano_acao,
+      id_executor,
+      id_plano_trabalho,
+      id_meta,
+      sequencial_meta,
+      codigo_emenda_parlamentar_formatado_plano_acao,
+      cnpj_beneficiario_plano_acao,
+      nome_beneficiario_plano_acao,
+      uf_beneficiario_plano_acao,
+      cnpj_executor,
+      nome_executor,
+      objeto_executor,
+      situacao_plano_trabalho,
+      prazo_execucao_meses_plano_trabalho,
+      classificacao_orcamentaria_pt,
+      desc_meta,
+      qt_meses_meta,
+      un_medida_meta,
+      qt_unidade_meta = qt_uniade_meta,
+      #
+      valor_custeio_plano_acao,
+      valor_investimento_plano_acao,
+      #
+      vl_custeio_executor,
+      vl_investimento_executor,
+      #
+      vl_custeio_emenda_especial_meta,
+      vl_investimento_emenda_especial_meta,
+      #
+      vl_custeio_recursos_proprios_meta,
+      vl_investimento_recursos_proprios_meta,
+      #
+      vl_custeio_doacao_meta,
+      vl_investimento_doacao_meta,
+      #
+      vl_custeio_rendimento_meta,
+      vl_investimento_rendimento_meta
 
-  )
+    )
+
+}
+
+emendas_programa_23 <- consolida_emendas_por_programas("23")
+emendas_programa_24 <- consolida_emendas_por_programas("24")
+
+emendas <- bind_rows(emendas_programa_23, emendas_programa_24)
 
 saveRDS(emendas, PATH_MINI_TRANSFEREGOV)
 write_excel_csv2(emendas, "tasks\\analise-exploratoria\\inputs\\emendas-detalhadas-fevereiro-2026.csv")
