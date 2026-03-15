a01141ec64f2a2380df40291e69368654394524d
rdurl0 <durlo.raul@gmail.com>
2026-03-15 17:35:32 -0300

    feat: adiciona impressão das tabelas do banco de dados e visualização dos empenhos

--- tasks/db/src/R/02-analise-minima.R
+++ tasks/db/src/R/02-analise-minima.R
@@ -13,4 +13,11 @@ con <- conectar_transferegov()
 df_empenhos <- get_query("SELECT * FROM empenho LIMIT 5", con = con)
 df_pagamentos <- get_query("SELECT * FROM documento_habil LIMIT 5", con = con)
 
+# Ver todas as tabelas do banco de dados
+tabelas <- DBI::dbListTables(con)
+
+print(tabelas)
+glimpse(df_empenhos)
+
+
 DBI::dbDisconnect(con, shutdown = TRUE)
